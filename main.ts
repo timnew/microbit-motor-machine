f29afcee8364a9d3d771e8083a87044f01d167b7
TimNew <timnew@users.noreply.github.com>
2021-10-26 10:07:52 +1100

    Add a bit color for RGB, seems broke the compatibility with blocks

--- main.ts
+++ main.ts
@@ -7,6 +7,11 @@ radio.onReceivedValue(function (name, value) {
 })
 let angle = 0
 let motrorStates = 0
+function setMotor(motor: SuperBit.enMotors, enable: boolean, color: NeoPixelColors) {
+    SuperBit.RGB_Program().showColor(neopixel.colors(color))
+    SuperBit.RGB_Program().show()
+    SuperBit.MotorRun(motor, enable? 255:0)
+}
 radio.setGroup(1)
 radio.setTransmitPower(7)
 angle = 0
@@ -14,8 +19,8 @@ motrorStates = 0
 basic.showIcon(IconNames.Heart)
 basic.forever(function () {
     SuperBit.Servo2(SuperBit.enServo.S1, angle)
-    SuperBit.MotorRun(SuperBit.enMotors.M1, (motrorStates & 0x01) > 0? 255:0)
-    SuperBit.MotorRun(SuperBit.enMotors.M2, (motrorStates & 0x02) > 0 ? 255 : 0)
-    SuperBit.MotorRun(SuperBit.enMotors.M3, (motrorStates & 0x04) > 0 ? 255 : 0)
-    SuperBit.MotorRun(SuperBit.enMotors.M4, (motrorStates & 0x08) > 0 ? 255 : 0)
+    setMotor(SuperBit.enMotors.M1, (motrorStates & 0x01) > 0, NeoPixelColors.Red)
+    setMotor(SuperBit.enMotors.M2, (motrorStates & 0x02) > 0, NeoPixelColors.Green)
+    setMotor(SuperBit.enMotors.M3, (motrorStates & 0x04) > 0, NeoPixelColors.Blue)
+    setMotor(SuperBit.enMotors.M4, (motrorStates & 0x08) > 0, NeoPixelColors.Yellow)
 })
